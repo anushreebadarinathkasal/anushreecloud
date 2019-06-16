42903fae03033f28114b3a5ff283c43915e20bb8
anushreebadarinathkasal <43047233+anushreebadarinathkasal@users.noreply.github.com>
2019-06-16 16:27:41 -0500

    hi

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import random
 
 app = Flask(__name__)
 
-port = int(os.getenv('PORT', 6000))
+port = int(os.getenv('PORT', 7000))
 
 
 myHostname = "anushreeazure.redis.cache.windows.net"
@@ -74,33 +74,36 @@ def randomcache():
 def randomrange():
     j = 0
     start_time = time.time()
+    countCache = 0
+    countwithoutCache = 0
     for i in range(100):
-        mag = "{:.2f}".format(random.uniform(1, 8))
+        # mag = "{:.2f}".format(random.uniform(1, 8))
+        mag = round(random.uniform(1,8), 2)
         if r.exists(mag):
             isCache = 'with Cache'
             print(isCache,mag)
-
+            countCache += 1
+            print(countCache)
             rows = cPickle.loads(r.get(mag))
             # end_time = time.time() - start_time
             # r.delete(mag)
         else:
 
             isCache = 'without Cache'
+            countwithoutCache += 1
             #start_time = time.time()
             con = sql.connect("database.db")
             cur = con.cursor()
             mag1 = str(random.uniform(1, 8))
-            mag2 = str(random.uniform(1, 8))
-            print(isCache, mag1, mag2)
-            cur.execute("select * from Earthquake where mag between " + mag1 + " and " + mag2)
+            print(isCache, mag1)
+            cur.execute("select * from Earthquake where mag>="+mag1)
             rows = cur.fetchall();
-
             con.close()
             r.set(mag, cPickle.dumps(rows))
         j = j+1
         end_time = time.time() - start_time
     print(j)
-    return render_template('results.html', data=rows, time=end_time, isCache=isCache)
+    return render_template('results.html', data=rows, time=end_time, isCache=isCache, cc= countCache, cc1=countwithoutCache )
 
 
 
@@ -134,6 +137,7 @@ def upload_csv():
     return render_template('upload.html')
 
 
+
 @app.route('/addrec', methods=['POST', 'GET'])
 def addrec():
     if request.method == 'POST':
@@ -152,11 +156,36 @@ def list():
     start_time = time.time()
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute("select * from Earthquake")
+    cur.execute("select * from quakes")
     rows = cur.fetchall();
     end_time=time.time()-start_time
     con.close()
-    return render_template("list.html", rows=rows, time=end_time)
+    return render_template("list.html", rows=rows, time=end_time, count=(len(rows)))
+
+@app.route('/addnewrec')
+def addnewrec():
+    return render_template('uploads.html')
+
+@app.route('/addnewrec1', methods=['POST', 'GET'])
+def addnewrec1():
+    if request.method == 'POST':
+        con = sql.connect("database.db")
+        csv = request.files['myfile']
+        file = pd.read_csv(csv)
+        file.to_sql('quakes', con, schema=None, if_exists='replace', index=True, index_label=None, chunksize=None,
+                    dtype=None)
+        con.close()
+        return render_template("result.html", msg="Record inserted successfully")
+
+@app.route('/delete')
+def delete():
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("delete from quakes  where rms <=0.25")
+    con.close()
+    return render_template("list.html")
+
+
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=port,debug=True)
